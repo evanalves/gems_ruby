2c94be63dd25b737ae9fe512d8504d06a5832208
OEM Configuration (temporary user) <everton.alves.luz@hotmail.com>
2024-05-05 13:22:09 -0300

    Teste primário

--- os.rb
+++ os.rb
@@ -3,4 +3,13 @@ require 'os'
 def my_os
     if OS.windows?
         "windos"
-    elsif OS.linux?    
+    elsif OS.linux?
+        "Linux"
+    elsif OS.mac
+        "Osx"
+    else
+        "Não consegui identificar"
+    end
+end
+
+puts "Meu PC possui #{OS.cpu_count} cores, é #{OS.bits} bits e o sistema operacional é #{}"
